bdc232e8b039eb671f57ba52afe1e83761a46560
Luiz Fernando <luizfgrocha04@gmail.com>
2022-08-25 23:46:57 -0300

    Esboco iniciado, tabuleiro inicializado

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+/*
+
+Esboco: 
+
+1) Inicializar um tabuleiro 8x8, preencher as casas com 0.
+
+2) Fazer funções que descrevem os possíveis movimentos do cavalo, incluindo critérios para decidir sobre a sua validade.
+
+
+Detalhes: 
+
+    as coordenadas do tabuleiro estao dadas como em uma matriz, na ordem [linha][coluna].
+
+
+Brainstorm:
+
+    se o movimento tiver sucesso, ele retorna 1. isso deve ser suficiente para
+    saber a nova posicao e aumentar a quantidade de casas em que o cavalo andou
+
+*/
+
+
+int main(){
+
+    int tabuleiro[8][8] = {0}; // inicializa o tabuleiro e preenche todas as casas com 0
+
+    
+}
