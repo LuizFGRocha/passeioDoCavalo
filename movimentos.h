8784149ad1901cb331262adaf5b8025a093bad89
Luiz Fernando <luizfgrocha04@gmail.com>
2022-08-26 00:05:31 -0300

    Header do movimentos.c

---
+++ movimentos.h
@@ -0,0 +1,15 @@
+int cimaDireita(int tabuleiro[][8], int m, int n);
+
+int direitaCima(int tabuleiro[][8], int m, int n);
+
+int direitaBaixo(int tabuleiro[][8], int m, int n);
+
+int baixoDireita(int tabuleiro[][8], int m, int n);
+
+int baixoEsquerda(int tabuleiro[][8], int m, int n);
+
+int esquerdaBaixo(int tabuleiro[][8], int m, int n);
+
+int esquerdaCima(int tabuleiro[][8], int m, int n);
+
+int cimaEsquerda(int tabuleiro[][8], int m, int n);
