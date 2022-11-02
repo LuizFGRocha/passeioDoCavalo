b217b424f1bb1df23827a2ef57edef0bca431b83
Luiz Fernando <luizfgrocha04@gmail.com>
2022-11-02 02:26:10 -0300

    Incluído timer. main recebe casa antes de compilar

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include "passeio.h"
+#include <time.h>
 
 int main() {
     int linha, coluna;
-    scanf("%d %d", &linha, &coluna);
-    fazerPasseio(linha, coluna);
+    clock_t comeco = clock();
+    fazerPasseio(8, 8);
+    clock_t final = clock();
+    printf("%lf\n", (double) (final - comeco) / CLOCKS_PER_SEC);
     return 0;
 }
